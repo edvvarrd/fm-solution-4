a881c4290e2802c1df6c009cfa12df2dc471233b
Kamil K <110562637+edvvarrd@users.noreply.github.com>
2023-08-04 14:18:57 +0300

    Changed vars to consts

--- js/map.js
+++ js/map.js
@@ -1,4 +1,4 @@
-var map = L.map('map', {
+const map = L.map('map', {
 	center: [41.48134126987655, -71.3103655846963],
 	zoom: 16,
 	zoomControl: false,
@@ -8,9 +8,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
 	maxZoom: 19,
 	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
 }).addTo(map)
-var myIcon = L.icon({
+const myIcon = L.icon({
 	iconUrl: './img/icon-location.svg',
 	iconSize: [66, 88],
 	iconAnchor: [33, 88],
 })
-var marker = L.marker([41.48134126987655, -71.3103655846963], { icon: myIcon }).addTo(map)
+const marker = L.marker([41.48134126987655, -71.3103655846963], { icon: myIcon }).addTo(map)
